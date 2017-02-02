fdb0a92ddab7cbab6ba3187d3f3480c1b6f35866
willy <willyjunir91@gmail.com>
2017-02-02 10:08:29 +0100

    ecole

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,17 +8,31 @@
  *
  * Main module of the application.
  */
- var app = angular.module('mobileApp', []);
+ var mobileApp = angular.module('mobileApp', []);
 
-app.controller("headerCtrl",function ($scope) {
+mobileApp.controller("headerCtrl",function ($scope) {
+   $scope.articles = [{"name" : "Telephone sans-fil", "quantity":1, "price":"29.99"}, {"name":"chargeur samsung", "quantity":1,"price":"29.99"}];
+  $scope.total = function () {
+   var total =0;
+   for (var i = 0; i < $scope.articles.length;i++){
+    total += $scope.articles[i].price * $scope.articles[i].quantity;
+   }
+   return total;
+  };
+  function calculateDiscount(newValue, oldValue, scope) {
+    $scope.discount = (newValue > 100) ? newValue * 0.10:0;
+  };
+    $scope.finalTotal = function () {
+    return $scope.total() - $scope.discount;
+  };
+ $scope.$watch($scope.total, calculateDiscount);
+});
+mobileApp.controller("menuCtrl",function ($scope) {
 
 });
-app.controller("menuCtrl",function ($scope) {
+mobileApp.controller("contentCtrl",function ($scope) {
 
 });
-app.controller("contentCtrl",function ($scope) {
-
-});
-app.controller("footerCtrl",function ($scope) {
+mobileApp.controller("footerCtrl",function ($scope) {
 
 });
